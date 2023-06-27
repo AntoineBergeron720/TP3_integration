9c4e32860f2d49a0d352a13972826b9f8b2f39a9
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2023-06-27 00:21:09 -0400

    progress on the styling of the table

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import MyProductTable from "@/components/molecules/myProductArray/myProductTable";
 import { Box } from "@mui/material";
 
 export default function CategoryPage() {
   return (
-    <Box>
-
+    <Box sx={{ padding: "5%" }}>
+      <MyProductTable
+        myProductArray={[
+          {
+            id: 1,
+            name: "Coca-Cola",
+            category: "Boissons",
+            price: 1.5,
+          },
+          {
+            id: 2,
+            name: "Pepsi",
+            category: "Boissons",
+            price: 1.5,
+          },
+          {
+            id: 3,
+            name: "7up",
+            category: "Boissons",
+            price: 1.5,
+          },
+        ]}
+      />
     </Box>
   );
 }
--- src/components/molecules/myProductArray/myProductTable.tsx
+++ src/components/molecules/myProductArray/myProductTable.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-import { Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 interface MyProductTableProps {
   myProductArray: any; // Change to the api category type
@@ -8,21 +19,68 @@ interface MyProductTableProps {
 
 export default function MyProductTable(props: MyProductTableProps) {
   const columns = [
-    { field: 'name', headerName: 'Product Name', width: 130 },
-    { field: 'category', headerName: 'Category', width: 130},
-    { field: 'price', headerName: 'Price', width: 90 }
+    { field: "name", headerName: "Produit" },
+    { field: "category", headerName: "Categorie" },
+    { field: "price", headerName: "Prix ($)" },
   ];
-  
+
   return (
     <TableContainer>
       <Table>
         <TableHead>
-          <TableRow>
+          <TableRow sx={{ display: "flex", alignItems: "flex-start", alignSelf: "stretch" }}>
             {columns.map((column) => (
-              <TableCell key={column.field}>{column.headerName}</TableCell>
+              <TableCell key={column.field} sx={{ display: "flex", padding: "0px 10px", alignItems: "center", flex: "1 0 0", alignSelf: "stretch" }}>
+                <Typography sx={{ fontSize: "34px", color: "primary.main" }}>
+                  {column.headerName}
+                </Typography>
+              </TableCell>
             ))}
+            {/* <TableCell sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", flex: "1 0 0", alignSelf: "stretch" }}> */}
+            <TableCell sx={{ display: "flex", padding: "8px 22px", flexDirection: "column", justifyContent: "center", alignItems: "center", flex: "1 0 0", alignSelf: "stretch" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                href="/categories/new"
+              >
+                <Typography sx={{ display: "flex", alignContent: "center" }}>
+                  Ajouter un produit{" "}
+                  <AddBoxIcon
+                    sx={{
+                      borderRadius: "5px",
+                      marginLeft: "5px"
+                    }}
+                  />
+                </Typography>
+              </Button>
+            </TableCell>
           </TableRow>
         </TableHead>
+        <TableBody>
+          {props.myProductArray
+            ? props.myProductArray.map((product) => (
+                <TableRow key={product.id} sx={{ display: "flex", alignItems: "center", alignSelf: "stretch" }}>
+                  {columns.map((column) => (
+                    <TableCell key={column.field}>
+                      {/* <Typography sx={{ fontSize: "24px", lineHeight: "30px", letter-spacing: 0.5px; }}> */}
+                      <Typography sx={{ fontSize: "24px"}}>
+                        {product[column.field]}
+                      </Typography>
+                    </TableCell>
+                  ))}
+                  <TableCell sx={{ display: "flex", justifyContent: "center" }}>
+                    {/* <Button
+                      variant="contained"
+                      color="primary"
+                      action={null} // to change for the function that gonna delete
+                    > */}
+                      <DeleteForeverIcon />
+                    {/* </Button> */}
+                  </TableCell>
+                </TableRow>
+              ))
+            : null}
+        </TableBody>
       </Table>
     </TableContainer>
   );
