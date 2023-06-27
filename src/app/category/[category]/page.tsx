41b0fb42798f6a7745c5275623cf814a9513936a
Andréann Poirier <poirierann@gmail.com>
2023-06-27 15:53:16 -0400

    formulaires ajouter/modifier catégorie

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import MyPageTitle from "@/components/molecules/title/my-page-title";
-import MyFormCategory from '@/components/molecules/form/my-form-category';
-import { Box } from '@mui/material';
+import MyFormCategory from "@/components/molecules/form/my-form-category";
+import { Box, Container } from "@mui/material";
 
 export default function AddCategory() {
-    return (
-      <Box sx={{ backgroundColor: "white" }}>
-        <MyPageTitle title="Modifier une catégorie"/>
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        backgroundColor: "white",
+        flexDirection: "column",
+        minWidth: 300,
+        mt: 2,
+      }}
+      fixed
+      maxWidth="sm"
+    >
+      <Box sx={{ mb: 10 }}>
+        <MyPageTitle title="Modifier une catégorie" />
+      </Box>
+      <Box>
         <MyFormCategory name="test" />
       </Box>
-    );
-  }
+    </Container>
+  );
+}
