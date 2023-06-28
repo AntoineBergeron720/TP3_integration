38a0fa65356b87eb8666412a33eeb259e3456c24
Charles-Étienne "Brie" Cloutier <charles.etienne.cloutier96@gmail.com>
2023-06-28 17:00:46 -0400

    Remade structure

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,28 +1,34 @@
-'use client'
+"use client";
 
 import { Categories } from "@/types/modules";
 import { Products } from "@/types/modules";
 import { getData } from "../common/jeuxApi";
 
-
-
 export default async function ProductPage() {
-  const productsData = await getData('https://api-tp3-integration.onrender.com/products')
-  const categoriesData = await getData('https://api-tp3-integration.onrender.com/categories')
+  const productsData = await getData(
+    "https://api-tp3-integration.onrender.com/products"
+  );
+  const categoriesData = await getData(
+    "https://api-tp3-integration.onrender.com/categories"
+  );
 
- // for each categoryId in Products, find a matching string in Categories and return the name
-
- for (let i = 0; i < productsData.products.length; i++) {
+  for (let i = 0; i < productsData.products.length; i++) {
     for (let j = 0; j < categoriesData.categories.length; j++) {
-      if (productsData.products[i].categoryId == categoriesData.categories[j]._id) {
-        productsData.products[i].categoryId = categoriesData.categories[j].name
-      } 
+      if (
+        productsData.products[i].categoryId == categoriesData.categories[j]._id
+      ) {
+        productsData.products[i].categoryId = categoriesData.categories[j].name;
+      }
     }
   }
-
+  // With this loop, we can now display the name of the category instead of the id
   return (
     <ul>
-        {productsData.products.map((product: Products) => (<li>{product.title} : {product.categoryId}</li>))}
+      {productsData.products.map((product: Products) => (
+        <li>
+          {product.title} : {product.categoryId}
+        </li>
+      ))}
     </ul>
   );
 }
