ad0294d728bb4d95c45df823b0a05d43f3c3983b
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2023-07-01 14:59:42 -0400

    commit to change branch

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import MyProductTable from "@/components/molecules/myProductArray/myProductTable";
 import { Box } from "@mui/material";
 import { getData } from "../common/jeuxApi";
-import { Category } from "@mui/icons-material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Categories } from "@/types/modules";
+import MyPageTitle from "@/components/molecules/title/my-page-title";
+import MyCategoryTable from "@/components/molecules/myCategoryArray/myCategoryArray";
 
 
 
 export default async function CategoryPage() {
-  const data = await getData('https://api-tp3-integration.onrender.com/categories')
-  const total = data.categories.length;
+  const [categories, setCategories] = useState<Categories[]>([]);
+  //const data = await getData('https://api-tp3-integration.onrender.com/categories');
+
+  useEffect(() => {
+    getCategories();
+  }, [categories]);
+
+  function getCategories() {
+    getData('https://api-tp3-integration.onrender.com/categories')
+      .then(data => {
+        setCategories(data.categories);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <Box>
-      
+      <MyPageTitle title="Catégories" />
+
+      <MyCategoryTable categories={categories} />
     </Box>
   );
 }
