885752f6d7d150f458685573d404e7a0ae91ff4d
asm22ae <117685069+asm22ae@users.noreply.github.com>
2023-07-05 14:40:24 -0400

    changment de page.tsx

---
+++ src/app/[locale]/category/new/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import MyPageTitle from "@/components/molecules/title/my-page-title";
+import MyFormCategory from "@/components/molecules/my-form-category/my-form-category";
+import { Box, Container } from "@mui/material";
+
+export default function AddCategory() {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minWidth: 300,
+        mt: 2,
+      }}
+      fixed
+      maxWidth="sm"
+    >
+      <Box sx={{ mb: 10 }}>
+        <MyPageTitle title="Ajouter une catégorie" />
+      </Box>
+      <Box>
+        <MyFormCategory name="test" />
+      </Box>
+    </Container>
+  );
+}
