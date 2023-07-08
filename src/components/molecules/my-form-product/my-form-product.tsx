0cb5c99f0a8ac739fa6f062d36991ffa5bd70150
AntoineBergeron720 <antoine.bergeron1@hotmail.com>
2023-07-08 05:52:31 -0400

    update page produit et modifier produit

--- src/components/molecules/my-form-product/my-form-product.tsx
+++ src/components/molecules/my-form-product/my-form-product.tsx
@@ -1,15 +1,15 @@
 import { Grid, TextField, FormControl, InputLabel, MenuItem } from "@mui/material";
 import MyButtonCancel from "../../atoms/button/my-button-cancel";
 import MyButtonSave from "../../atoms/button/my-button-save";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export function MyFormCreateProduct() {
 
     const [categoryList, setCategoryList] = useState<any[]>([])
 
-    useEffect(()=>{
-        let response = [{_id: '1', name: 'cat 1'}, {_id: '2', name: 'cat 2'}] // TODO : REQUÊTE À L'API
+    useEffect(() => {
+        let response = [{ _id: '1', name: 'cat 1' }, { _id: '2', name: 'cat 2' }] // TODO : REQUÊTE À L'API
         // TODO : Validation code response, si 200 -> setCategoryList avec liste retourné par l'API, si erreur, toast.erreur
         setCategoryList(response)
     }, [])
@@ -20,7 +20,7 @@ export function MyFormCreateProduct() {
         setCategory(event.target.value as string);
     };
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log(category)
     }, [category])
 
@@ -28,7 +28,7 @@ export function MyFormCreateProduct() {
         <Grid container rowGap={3} columnGap={2}>
             <Grid item xs={12}>
                 <TextField
-                    id="ditre"
+                    id="titre"
                     label="Titre"
                     variant="outlined"
                     fullWidth
@@ -73,18 +73,18 @@ export function MyFormCreateProduct() {
                         mt: 5
                     }}
                 />
-                <FormControl 
-                fullWidth
-                sx={
-                    {
-                        mt: 5,
-                       
-                        "& fieldset": {
-                            border: "2px solid #007FFF"
-                            
+                <FormControl
+                    fullWidth
+                    sx={
+                        {
+                            mt: 5,
+
+                            "& fieldset": {
+                                border: "2px solid #007FFF"
+
+                            }
                         }
                     }
-                }
                 >
                     <InputLabel id="category">Catégorie</InputLabel>
                     <Select
@@ -93,7 +93,7 @@ export function MyFormCreateProduct() {
                         value={category}
                         label="Catégorie"
                         onChange={handleChange}
-                        
+
                     >
                         {
                             categoryList && categoryList.map((cat) => {
@@ -121,17 +121,71 @@ export function MyFormCreateProduct() {
 }
 
 interface MyFormUpdateProductProps {
-    name: string;
+    titre: string,
+    description: string,
+    prix: string,
+    category: string
 }
 
 export function MyFormUpdateProduct(props: MyFormUpdateProductProps) {
+    const [categoryList, setCategoryList] = useState<any[]>([])
+
+    useEffect(() => {
+        let response = [{ _id: '1', name: 'cat 1' }, { _id: '2', name: 'cat 2' }] // TODO : REQUÊTE À L'API
+        // TODO : Validation code response, si 200 -> setCategoryList avec liste retourné par l'API, si erreur, toast.erreur
+        setCategoryList(response)
+    }, [])
+
+    const [category, setCategory] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setCategory(event.target.value as string);
+    };
+
+    useEffect(() => {
+        console.log(category)
+    }, [category])
+
     return (
         <Grid container rowGap={3} columnGap={2}>
             <Grid item xs={12}>
                 <TextField
-                    id="name"
-                    label="Name"
-                    value={props.name}
+                    id="titre"
+                    label="Nouveau titre"
+                    value={props.titre}
+                    variant="outlined"
+                    fullWidth
+                    sx={{
+                        "& .MuiOutlinedInput-root": {
+                            "& fieldset": {
+                                border: "2px solid #007FFF",
+                                borderRadius: "2px",
+                            },
+                        }
+                    }}
+                />
+                <TextField
+                    id="description"
+                    label="Nouvelle description"
+                    value={props.description}
+                    variant="outlined"
+                    multiline
+                    minRows={5}
+                    fullWidth
+                    sx={{
+                        "& .MuiOutlinedInput-root": {
+                            "& fieldset": {
+                                border: "2px solid #007FFF",
+                                borderRadius: "2px",
+                            },
+                        },
+                        mt: 5
+                    }}
+                />
+                <TextField
+                    id="prix"
+                    label="Nouveau prix"
+                    value={props.prix}
                     variant="outlined"
                     fullWidth
                     sx={{
@@ -141,8 +195,39 @@ export function MyFormUpdateProduct(props: MyFormUpdateProductProps) {
                                 borderRadius: "2px",
                             },
                         },
+                        mt: 5
                     }}
                 />
+                <FormControl
+                    fullWidth
+                    sx={
+                        {
+                            mt: 5,
+
+                            "& fieldset": {
+                                border: "2px solid #007FFF"
+
+                            }
+                        }
+                    }
+                >
+                    <InputLabel id="category">Catégorie</InputLabel>
+                    <Select
+                        labelId="category"
+                        id="categorySelect"
+                        value={props.category}
+                        label="Nouvelle catégorie"
+                        onChange={handleChange}
+
+                    >
+                        {
+                            categoryList && categoryList.map((cat) => {
+                                return <MenuItem key={cat._id} value={cat._id}>{cat.name}</MenuItem>
+                            })
+                        }
+
+                    </Select>
+                </FormControl>
             </Grid>
             <Grid
                 item
